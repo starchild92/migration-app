f10be8ce355c5c068d5f48583679d36233458d80
Luis Pérez <la.perez@pappcorn.com>
2019-01-14 16:02:34 -0500

    <CHORE> Reading a XML File from input

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,30 @@
 import { Component } from '@angular/core';
 
+var xml2js = require('xml2js');
+
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+	selector: 'app-root',
+	templateUrl: './app.component.html',
+	styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+	title = 'app';
+
+	openfile(file) {
+		console.log(file)
+		let parser = new xml2js.Parser();
+		let reader = new FileReader()
+		let onload = function (event) {
+
+			console.log(event)
+
+			let text = reader.result
+			parser.parseString(text, function (err, resp) {
+				console.dir(resp)
+				console.log('Done')
+			})
+		}
+		reader.onload = onload
+		reader.readAsText(file.target.files[0])
+	}
 }
